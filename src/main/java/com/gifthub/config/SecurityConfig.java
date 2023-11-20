8f13b3df7c91692623c303cd6fbd4358afc30312
9min9 <gming0226@gmail.com>
2023-11-20 14:25:20 +0900

    test: Spring security test
    Spring security의 permitAll()을 적용한 테스트 코드 적용

--- src/main/java/com/gifthub/config/SecurityConfig.java
+++ src/main/java/com/gifthub/config/SecurityConfig.java
@@ -13,8 +13,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @RequiredArgsConstructor
@@ -34,6 +41,8 @@ public class SecurityConfig  {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+
+    //TEST
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -42,29 +51,41 @@ public class SecurityConfig  {
                 .and()
 
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers(
-                                new AntPathRequestMatcher("/css/**"),
-                                new AntPathRequestMatcher("/js/**"),
-                                new AntPathRequestMatcher("/images/**"),
-                                new AntPathRequestMatcher("/scss/**"),
-                                new AntPathRequestMatcher("/webfonts/**")
-
-                        ).permitAll()
-
                         .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                        .requestMatchers("/","/signup", "/error", "/login", "/login/index" , "api/kakao/**" ).permitAll()
-                        .anyRequest().authenticated()
-                )
-
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, kakaoUserJwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new KakaoJwtAuthenticationFilter(kakaoUserJwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class);
+                        .anyRequest().permitAll()
+                );
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         //	.httpBasic();
         return http.build();
     }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//                .formLogin()
+//                .loginPage("/login")
+//                .and()
+//
+//                .authorizeHttpRequests((authz) -> authz
+//                        .requestMatchers(
+//                                new AntPathRequestMatcher("/css/**"),
+//                                new AntPathRequestMatcher("/js/**"),
+//                                new AntPathRequestMatcher("/images/**"),
+//                                new AntPathRequestMatcher("/scss/**"),
+//                                new AntPathRequestMatcher("/webfonts/**")
+//                        ).permitAll()
+//
+//                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+//                        .requestMatchers("/","/signup", "/error", "/login", "/login/index" , "api/kakao/**" ).permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+//
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        //	.httpBasic();
+//        return http.build();
+//    }
 
 
 
