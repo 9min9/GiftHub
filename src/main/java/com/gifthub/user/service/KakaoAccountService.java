e52329a24b7d512fcf7a979c17544bc33736e89a
9min9 <gming0226@gmail.com>
2023-11-20 14:51:38 +0900

    test : KakaoAccountService test 적용

--- src/main/java/com/gifthub/user/service/KakaoAccountService.java
+++ src/main/java/com/gifthub/user/service/KakaoAccountService.java
@@ -1,26 +1,27 @@
 package com.gifthub.user.service;
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.time.LocalDate;
-import java.util.HashMap;
 
 import com.gifthub.user.dto.KakaoUserDto;
-import com.gifthub.user.dto.UserDto;
 import com.gifthub.user.entity.KakaoUser;
 import com.gifthub.user.entity.User;
 import com.gifthub.user.repository.KakaoUserRepository;
-import com.google.gson.*;
+import com.gifthub.user.repository.UserRepository;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Service
 @RequiredArgsConstructor
 public class KakaoAccountService {
 
     private final KakaoUserRepository kakaoUserRepository;
+    private final UserRepository userRepository;
     @Value("${kakaoRestKey}")
     private String restKey;
 
@@ -29,8 +30,12 @@ public class KakaoAccountService {
 
         return kakaoUser.toKakaoUserDto();
     }
-    public KakaoUserDto getUser(String kakaoAccountId){
-        KakaoUser kakaoUser = kakaoUserRepository.findById(kakaoAccountId).orElse(null);
+    public KakaoUserDto getKakaoUser(Long userId, String kakaoId){
+
+        //TODO : update repostiory
+        User kakaoUser = userRepository.findById(userId).orElse(null);
+//        userRepository.findByKakao(kakaoAccountId)
+//        KakaoUser kakaoUser = kakaoUserRepository.findById(kakaoAccountId).orElse(null);
 
         if(kakaoUser==null){
             return  null;
@@ -39,7 +44,7 @@ public class KakaoAccountService {
 
     }
     public void delUser(String kakaoAccountId){
-        kakaoUserRepository.deleteById(kakaoAccountId);
+//        kakaoUserRepository.deleteById(kakaoAccountId);
     }
 
 
@@ -107,7 +112,7 @@ public class KakaoAccountService {
 
     public KakaoUserDto getKakaoUserInfo (String access_Token) {
         //    요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
-        HashMap<String, Object> userInfo = new HashMap<>();
+//        HashMap<String, Object> userInfo = new HashMap<>();
         String reqURL = "https://kapi.kakao.com/v2/user/me";
 
         try {
@@ -117,10 +122,12 @@ public class KakaoAccountService {
             //    요청에 필요한 Header에 포함될 내용
             // Bearer => JWT 혹은 OAuth에 대한 토큰을 사용한다. (RFC 6750)
             conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+
             int responseCode = conn.getResponseCode();
             System.out.println("access_token :" + access_Token );
             System.out.println("responseCode : " + responseCode);
 
+            //Json을 Pasing하기 위함
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String line = "";
             String result = "";
@@ -128,35 +135,13 @@ public class KakaoAccountService {
                 result += line;
             }
             System.out.println("response body : " + result);
+
             JsonParser parser = new JsonParser();
             JsonElement element = parser.parse(result);
-
-            String kakaoAccountId = element.getAsJsonObject().get("id").getAsString();
             JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
             JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
 
-
-//            Set<Map.Entry<String, JsonElement>> entries = element.getAsJsonObject().entrySet();
-////            System.out.println("@@@");
-//            for (Map.Entry<String, JsonElement> entry : entries) {
-//                System.out.println(entry.getKey());
-////                JsonObject test = element.getAsJsonObject().get(entry.getKey()).getAsJsonObject();
-////                System.out.println(test);
-//            }
-//            JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
-//            for (Map.Entry<String, JsonElement> entry : properties.entrySet()) {
-//                String test = properties.getAsJsonObject().get(entry.getKey()).getAsString();
-//                System.out.println(test);
-//            }
-//            //객체Object는 키-값 쌍의 집합이다.
-//            //todo : Map 객체를 만들어서 아래의 jsonElement를 저장하고 한번 더 iter
-//            JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
-//
-//            for (Map.Entry<String, JsonElement> entry : kakao_account.entrySet()) {
-//                JsonElement test2 = kakao_account.getAsJsonObject().get(entry.getKey());
-//                System.out.println(entry.getKey()+ " | " +test2);
-//            }
-            System.out.println("id:"+ kakaoAccountId);
+            String kakaoAccountId = element.getAsJsonObject().get("id").getAsString();
             String nickname = properties.getAsJsonObject().get("nickname").getAsString();
             String email = kakao_account.getAsJsonObject().get("email").getAsString();
             String name = kakao_account.getAsJsonObject().get("name").getAsString();
@@ -166,7 +151,7 @@ public class KakaoAccountService {
             String phone_number = kakao_account.getAsJsonObject().get("phone_number").getAsString();
 
 
-            KakaoUserDto test = KakaoUserDto.builder()
+            KakaoUserDto kakaoUserDto = KakaoUserDto.builder()
                     .kakaoAccountId(kakaoAccountId)
                     .name(name)
                     .nickname(nickname)
@@ -176,19 +161,17 @@ public class KakaoAccountService {
                     .phoneNumber(phone_number)
                     .build();
 
-            System.out.println("---kakaoDto---");
-            System.out.println(test.getKakaoAccountId());
-            System.out.println(test.getEmail());
-            System.out.println(test.getName());
-            System.out.println(test.getNickname());
-            System.out.println(test.getGender());
+//            System.out.println("---kakaoDto---");
+//            System.out.println(kakaoUserDto.getKakaoAccountId());
+//            System.out.println(kakaoUserDto.getEmail());
+//            System.out.println(kakaoUserDto.getName());
+//            System.out.println(kakaoUserDto.getNickname());
+//            System.out.println(kakaoUserDto.getGender());
 
-
-            return test;
-
-
+            return kakaoUserDto;
         } catch (IOException e) {
             // TODO Auto-generated catch block
+            System.out.println("getKakaoUserInfo error ###");
 
             return null;
         }
@@ -202,12 +185,9 @@ public class KakaoAccountService {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Authorization", "Bearer " + access_Token);
-            System.out.println("access_Token:"+access_Token);
             int responseCode = conn.getResponseCode();
-            System.out.println("로그아웃 성공");
 
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
             String result = "";
             String line = "";
 
