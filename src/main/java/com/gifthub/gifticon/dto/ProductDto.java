df47e4ec7657f987cac42212d511d9e94ee06821
SMayor <abdortus159@naver.com>
2023-11-20 15:09:02 +0900

    Feat : "db에 Product 리스트 넣기"

--- src/main/java/com/gifthub/gifticon/dto/ProductDto.java
+++ src/main/java/com/gifthub/gifticon/dto/ProductDto.java
@@ -1,40 +1,38 @@
 package com.gifthub.gifticon.dto;
 
-import com.gifthub.gifticon.entity.Gifticon;
 import com.gifthub.gifticon.entity.Product;
 import com.querydsl.core.annotations.QueryProjection;
-import java.util.ArrayList;
-import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
 public class ProductDto {
-private Long id;
-private String name;
-private Long price;
-private String brandName;
+    private Long id;
+    private String name;
+    private Long price;
+    private String brandName;
 
 
-public Product toProductEntity(){
-    return Product.builder()
-            .id(this.id)
-            .name(this.name)
-            .price(this.price)
-            .brandName(this.brandName)
-            .build();
-}
-public ProductDto() {
-}
+    public Product toProductEntity() {
+        return Product.builder()
+                .id(this.id)
+                .name(this.name)
+                .price(this.price)
+                .brandName(this.brandName)
+                .build();
+    }
 
-@QueryProjection
-public ProductDto(Long id, String name, Long price, String brandName) {
-    this.id = id;
-    this.name = name;
-    this.price = price;
-    this.brandName = brandName;
-}
+    public ProductDto() {
+    }
+
+    @QueryProjection
+    public ProductDto(Long id, String name, Long price, String brandName) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.brandName = brandName;
+    }
 }
