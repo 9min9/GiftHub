64c431feef352659df468edcab012657bc145324
SMayor <abdortus159@naver.com>
2023-11-22 17:20:15 +0900

    Refactor : "GifticonDto @QueryProjection"

--- src/main/java/com/gifthub/gifticon/dto/GifticonDto.java
+++ src/main/java/com/gifthub/gifticon/dto/GifticonDto.java
@@ -3,14 +3,14 @@ package com.gifthub.gifticon.dto;
 import com.gifthub.gifticon.entity.Gifticon;
 import com.gifthub.payment.enumeration.SaleStatus;
 import com.gifthub.user.dto.UserDto;
-import lombok.AllArgsConstructor;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-@AllArgsConstructor
+
 @NoArgsConstructor
 @Builder
 @Getter
@@ -37,5 +37,15 @@ public class GifticonDto {
                 .price(this.price)
                 .build();
     }
-
+    @QueryProjection
+    public GifticonDto(Long id, UserDto user, String barcode, LocalDate due, String brandName, String productName, SaleStatus saleStatus, Long price) {
+        this.id = id;
+        this.user = user;
+        this.barcode = barcode;
+        this.due = due;
+        this.brandName = brandName;
+        this.productName = productName;
+        this.saleStatus = saleStatus;
+        this.price = price;
+    }
 }
