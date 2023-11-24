826abf140f6702808619f0d9e4dee85a8c342571
9min9 <gming0226@gmail.com>
2023-11-25 01:54:31 +0900

    fix: change login front logic
    로그인 성공 시 두가지 소셜 로그인 컨트롤러가 모두 호출되는 문제 해결
    naver login 시 jwt를 쿠키에 저장

--- src/main/resources/static/js/account/login.js
+++ src/main/resources/static/js/account/login.js
@@ -1,23 +1,15 @@
 const urlParams = new URL(location.href).searchParams;
 
 window.onload = function () {
-
-    getKakaoJwt()
-    getNaverJwt()
-
     Kakao.init('578c006810acb509f2ccc52277d13ec9'); //발급받은 키 중 javascript키를 사용해준다.
-    Kakao.isInitialized()
+    Kakao.isInitialized();
 
-    //TODO : getJwt가 카카오 실행 후 리다이렉트 시 호출되어야 함
+    if (urlParams.get("state") != null) {
+        getNaverJwt();
+    } else if (urlParams.get("code") != null){
+        getKakaoJwt();
+    }
 }
-
-function loginWithKakao() {
-    Kakao.Auth.authorize({
-        redirectUri: 'http://localhost:8081/login',
-    });
-}
-
-
 
 function getKakaoJwt() {
     const code = urlParams.get('code');                 //Kakao 로그인이 성공했을 때 얻는 인가 코드를 URL에서 추출
@@ -34,11 +26,6 @@ function getKakaoJwt() {
             let authorizationHeader = xhr.getResponseHeader("Authorization");
             let token = authorizationHeader.replace("Bearer ", "");
             localStorage.setItem("token", token);
-
-            let setCookie = () => {
-                document.cookie = "Authorization=" + localStorage.getItem("token");
-            }
-
             setCookie();
 
             window.location.href = "/";
@@ -51,28 +38,31 @@ function getKakaoJwt() {
     });
 }
 
+function getNaverJwt() {
+    const code = urlParams.get('code');
+    let param = '?code=' + code;
+    $.ajax({
+        type: "post",
+        url: "http://localhost:8081/api/naver/login" + param,
+        success: function (jsonData, status, xhr) {
+            alert("로그인 성공")
+            console.log(jsonData);
 
-    function getNaverJwt() {
-        const code = urlParams.get('code');
-        let param = '?code=' + code;
-        $.ajax({
-            type: "post",
-            url: "http://localhost:8081/api/naver/login" +param,
-            success: function(jsonData, status, xhr){
-                alert("로그인 성공")
-                console.log(jsonData);
+            let authorizationHeader = xhr.getResponseHeader("Authorization");
+            let token = authorizationHeader.replace("Bearer ", "");
+            localStorage.setItem("token", token);
+            setCookie();
 
-                let authorizationHeader = xhr.getResponseHeader("Authorization");
-                let token = authorizationHeader.replace("Bearer ", "");
-                localStorage.setItem("token", token);
+            window.location.href = "/";
+            // window.history.back();
+        },
+        error: function (error) {
+            console.log("로그인실패")
+        }
+    });
 
-                window.location.href = "/";
-                // window.history.back();
-            },
-            error:function(error) {
-                console.log("로그인실패")
-            }
-        });
+}
 
-    }
-
+function setCookie() {
+    document.cookie = "Authorization=" + localStorage.getItem("token");
+}
