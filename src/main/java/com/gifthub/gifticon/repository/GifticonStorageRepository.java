0c3d1feb5c9e00583a064c05feba666883343a95
SMayor <abdortus159@naver.com>
2023-11-25 20:58:01 +0900

    Rename : "GifticonStorage"

---
+++ src/main/java/com/gifthub/gifticon/repository/GifticonStorageRepository.java
@@ -0,0 +1,7 @@
+package com.gifthub.gifticon.repository;
+
+import com.gifthub.gifticon.entity.GifticonStorage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GifticonStorageRepository extends JpaRepository<GifticonStorage, Long> {
+}
