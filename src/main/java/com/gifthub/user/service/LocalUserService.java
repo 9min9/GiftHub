1421b3e5c0d6cbb7ad36ae7f3f0764f7670d584f
jaehyeongP <asso03089@naver.com>
2023-11-28 12:15:03 +0900

    fix : localsignup dto, service

---
+++ src/main/java/com/gifthub/user/service/LocalUserService.java
@@ -0,0 +1,47 @@
+package com.gifthub.user.service;
+
+import com.gifthub.user.dto.LocalUserDto;
+import com.gifthub.user.dto.UserDto;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LocalUserService {
+
+    public UserDto getLocalUserDto(String data){
+
+
+    JsonParser parser = new JsonParser();
+    JsonElement element = parser.parse(data);
+    String email=element.getAsJsonObject().get("email").getAsString();
+    String password=element.getAsJsonObject().get("password").getAsString();
+    String name= element.getAsJsonObject().get("name").getAsString();
+    String gender = element.getAsJsonObject().get("gender").getAsString();
+    String nickname = element.getAsJsonObject().get("nickname").getAsString();
+    String tel = element.getAsJsonObject().get("tel").getAsString();
+    String birth = element.getAsJsonObject().get("birth").getAsString();
+    String year = birth.substring(0,4);
+    String birthday = birth.substring(4);
+
+        System.out.println("birth:"+birth);
+        System.out.println("birthday"+birthday);
+        System.out.println("year"+year);
+        System.out.println("tel"+tel);
+    UserDto userDto =UserDto.builder()
+            .password(password)
+            .email(email)
+            .name(name)
+            .gender(gender)
+            .nickname(nickname)
+            .tel(tel)
+            .date(birthday)
+            .year(year)
+            .build();
+
+
+    return userDto;
+}
+}
