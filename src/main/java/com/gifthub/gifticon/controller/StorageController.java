342308cd33d05a20228f13b660e5818d7a7b4b47
SMayor <abdortus159@naver.com>
2023-11-29 00:03:42 +0900

    Feat : "Storage 삭제, 이미지서버에서 삭제"

---
+++ src/main/java/com/gifthub/gifticon/controller/StorageController.java
@@ -0,0 +1,45 @@
+package com.gifthub.gifticon.controller;
+
+import com.gifthub.gifticon.entity.GifticonStorage;
+import com.gifthub.gifticon.service.GifticonImageService;
+import com.gifthub.gifticon.service.GifticonStorageService;
+import com.gifthub.user.UserJwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/storage")
+public class StorageController {
+    private final GifticonStorageService storageService;
+    private final GifticonImageService imageService;
+    private final UserJwtTokenProvider userJwtTokenProvider;
+
+
+    @PostMapping("/delete/{id}")
+    public ResponseEntity<Object> removeFromStorage(@PathVariable("id") Long storageId,
+                                                    @RequestHeader HttpHeaders headers){
+        try{
+            GifticonStorage storage = storageService.getStorageById(storageId);
+            Long userId = userJwtTokenProvider.getUserIdFromToken(headers.get("Authorization").get(0));
+
+            if (!storage.getUser().getId().equals(userId)) {
+
+                ResponseEntity.badRequest().build();
+            }
+
+            imageService.deleteFileByStorage(storage);
+
+        } catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok().build();
+
+    }
+
+
+}
