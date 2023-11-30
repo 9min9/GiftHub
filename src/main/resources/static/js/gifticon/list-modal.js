bb8c15cc4b30b4403f8cbf5bf42a868125ea1745
J Kim <jkim987891@gmail.com>
2023-11-30 22:15:15 +0900

    Feat: 장바구니 버튼 추가

--- src/main/resources/static/js/gifticon/list-modal.js
+++ src/main/resources/static/js/gifticon/list-modal.js
@@ -1,4 +1,6 @@
 function getOnSaleProduct(productId) {
+    document.querySelector("#price-container").innerHTML = "";
+
     let xhr = new XMLHttpRequest();
 
     xhr.open("get", "/api/gifticon/products/" + productId)
@@ -15,11 +17,35 @@ function getOnSaleProduct(productId) {
             let priceTd = document.createElement("td");
             let due = document.createElement("td");
 
+            let addToCartTd = document.createElement("td");
+            let addToCart = document.createElement("button");
+            addToCart.classList.add("btn--e-brand-b-2");
+            addToCart.dataset.gifticonId = p.id;
+
+            addToCart.innerText = "장바구니로";
+
+            addToCart.addEventListener("click", function(event) {
+                let xhr = new XMLHttpRequest();
+
+                xhr.open("post", "/api/carts");
+                xhr.setRequestHeader("Authorization", localStorage.getItem("token"));
+                xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+                xhr.onload = () => {
+                    // TODO 장바구니 추가
+                }
+
+                xhr.send("gifticonId=" + event.target.dataset.gifticonId);
+            });
+
+            addToCartTd.appendChild(addToCart);
+
             priceTd.innerText = p.price;
             due.innerText = p.due;
-
+            
             priceUnit.appendChild(priceTd);
             priceUnit.appendChild(due);
+            priceUnit.appendChild(addToCartTd);
 
             priceContainer.appendChild(priceUnit);
         }
