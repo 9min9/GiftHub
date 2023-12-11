c54e614a830abfde89141b4cc3e2dbf5aa7408cc
9min9 <gming0226@gmail.com>
2023-12-11 12:27:30 +0900

    fix: Change import
    package 이름 변경에 의한 fix

--- src/main/java/com/gifthub/user/controller/LocalUserAccountController.java
+++ src/main/java/com/gifthub/user/controller/LocalUserAccountController.java
@@ -2,7 +2,8 @@ package com.gifthub.user.controller;
 
 import com.gifthub.config.jwt.LocalUserAuthenticationProvider;
 import com.gifthub.config.jwt.SocialAuthenticationToken;
-import com.gifthub.global.util.ErrorResponse;
+import com.gifthub.global.error.ErrorResponse;
+import com.gifthub.global.exception.ExceptionResponse;
 import com.gifthub.user.UserJwtTokenProvider;
 import com.gifthub.user.dto.LocalUserDto;
 import com.gifthub.user.dto.SignupRequest;
@@ -21,6 +22,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -43,23 +47,22 @@ public class LocalUserAccountController {
     private final LocalUserAuthenticationProvider localUserAuthenticationProvider;
     private final LocalUserService localUserService;
     private final ErrorResponse errorResponse;
+    private final ExceptionResponse exceptionResponse;
 
     @PostMapping("/submit")
-    public ResponseEntity<Object> signup(@Valid  @RequestBody SignupRequest signupRequest, BindingResult bindingResult) {
+    public ResponseEntity<Object> signup(@Valid @RequestBody SignupRequest signupRequest, BindingResult bindingResult) {
         System.out.println("Submit Controller");
 
         try {
-            userService.saveLocalUser(signupRequest.toLocalUserDto());
+            if (userAccountService.validateDuplicateEmail(signupRequest.getEmail())) {
+                userService.saveLocalUser(signupRequest.toLocalUserDto());
 
-            if(userAccountService.isDuplicateEmail(signupRequest.getEmail())) {
-                bindingResult.rejectValue("email", "Duplicate.email");
             }
-
         } catch (DuplicateEmailException e) {
-            bindingResult.rejectValue("email", e.getCode());
+            bindingResult.rejectValue(e.getField(), e.getCode(), e.getMessage());
 
         } finally {
-            if(bindingResult.hasErrors()) {
+            if (bindingResult.hasErrors()) {
                 return ResponseEntity.badRequest().body(errorResponse.getErrors(bindingResult));
             }
 
@@ -68,20 +71,34 @@ public class LocalUserAccountController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody Map<String , String> request, Errors errors){
+    public ResponseEntity<Object> login(@RequestBody Map<String, String> request, Errors errors) {
         Map<String, String> result = new HashMap<>();
         System.out.println("LoginController");
-        String token="";
+        String token = "";
 
         try {
             String email = request.get("email");
             String password = request.get("password");
 
-            System.out.println("email:"+email);
-            System.out.println("password:"+password);
-            LoginValidator loginValidator = new LoginValidator();
-            loginValidator.validate(request, errors);
+            System.out.println("email:" + email);
+            System.out.println("password:" + password);
+//            LoginValidator loginValidator = new LoginValidator();
+//            loginValidator.validate(request, errors);
+
+            if (email == null || email.isEmpty()) {
+//                errors.rejectValue("email","NotBlank.email");
+//                errors.reject("NotBlank");
+                throw new NotFoundUserException();
+//                errors.rejectValue("email", "NotBlank.email", "");
+            }
+
+            if (password == null || password.isEmpty()) {
+//                errors.rejectValue("password", "NotBlank.password", "");
+            }
+
+            //todo : empty 일 때 reject 하고 리턴해야 함
             LocalUserDto localUserInfo = localUserService.getLocalUserByEmail(email);
+
 
             if (localUserInfo == null) {
                 throw new NotFoundUserException();
@@ -90,7 +107,7 @@ public class LocalUserAccountController {
             if (userAccountService.isLogin(email, password)) {
                 SocialAuthenticationToken LocalUserAuthenticationToken = new SocialAuthenticationToken(email);
                 Authentication authentication = localUserAuthenticationProvider.authenticate(LocalUserAuthenticationToken);
-                System.out.println("testauth:"+authentication);
+                System.out.println("testauth:" + authentication);
 
                 if (authentication.isAuthenticated()) {
                     LocalUserDto findLocalUserDto = localUserService.getLocalUserByEmail(localUserInfo.getEmail());
@@ -102,29 +119,42 @@ public class LocalUserAccountController {
                                     .id(localUserInfo.getId())
                                     .loginType(LoginType.GIFT_HUB.name()).build());
 
-                    System.out.println("testtoken:"+token);
+                    System.out.println("testtoken:" + token);
                 }
             }
 
         } catch (NotFoundUserException e) {
-            log.error("Login Controller | " +e);
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch(Exception e){
+            log.error("Login Controller | " + e);
+            Map<String, String> exception = exceptionResponse.getException("email", e.getCode(), e.getMessage());
+
+            return ResponseEntity.badRequest().body(exception);
+
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().build();
-        } finally {
-            if (errors.hasErrors()) {
-
-            }
-
         }
 
-        return ResponseEntity.ok().header( "Authorization", "Bearer " + token).build();
+        if (errors.hasErrors()) {
+            List<ObjectError> allErrors = errors.getAllErrors();
+            List<FieldError> fieldErrors = errors.getFieldErrors();
+
+            for (ObjectError allError : allErrors) {
+                System.out.println(allError.getClass() + " | " + allError.getObjectName() + " | " + allError.getCode() + " | ");
+            }
+
+            for (FieldError allError : fieldErrors) {
+                System.out.println(allError.getClass() + " | " + allError.getObjectName() + " | " + allError.getCode() + " | " + allError.getField());
+            }
+
+            return ResponseEntity.badRequest().body(errorResponse.getErrors(errors));
+        }
+
+        return ResponseEntity.ok().header("Authorization", "Bearer " + token).build();
     }
 
-    @PostMapping(value="/email/check")
-    public ResponseEntity<Object> emailCheck(@RequestBody Map<String, String> request){
-        Map<String,String> result = new HashMap<>();
+    @PostMapping(value = "/email/check")
+    public ResponseEntity<Object> emailCheck(@RequestBody Map<String, String> request) {
+        Map<String, String> result = new HashMap<>();
 
         try {
             String email = request.get("email");
@@ -137,8 +167,8 @@ public class LocalUserAccountController {
             result.put("status", "success");
             result.put("message", "사용 가능한 이메일 입니다");
 
-            if(email.isEmpty() || email==null){
-                result.put("message","");
+            if (email.isEmpty() || email == null) {
+                result.put("message", "");
             }
 
             return ResponseEntity.ok().body(result);
@@ -152,26 +182,26 @@ public class LocalUserAccountController {
         }
     }
 
-    @PostMapping(value="/password/check")
-    public ResponseEntity<Object> passwordCheck(@RequestBody Map<String, String> request){
-        Map<String,String> result = new HashMap<>();
+    @PostMapping(value = "/password/check")
+    public ResponseEntity<Object> passwordCheck(@RequestBody Map<String, String> request) {
+        Map<String, String> result = new HashMap<>();
 
         try {
 
             String passwrod = request.get("password");
-            String confirmPassword =request.get("confirmPassword");
+            String confirmPassword = request.get("confirmPassword");
             result.put("field", "confirmPassword");
 
 
-            if (!userAccountService.isMatchPasswordAndConfirmPassword(passwrod, confirmPassword )) {
+            if (!userAccountService.isMatchPasswordAndConfirmPassword(passwrod, confirmPassword)) {
                 throw new MismatchPasswordAndConfirmPassword();
             }
 
             result.put("status", "success");
             result.put("message", "비밀번호가 동일합니다.");
 
-            if(passwrod.isEmpty() || passwrod==null){
-                result.put("message","");
+            if (passwrod.isEmpty() || passwrod == null) {
+                result.put("message", "");
             }
 
 
@@ -187,24 +217,24 @@ public class LocalUserAccountController {
         }
     }
 
-    @PostMapping(value="/confirmpassword/check")
-    public ResponseEntity<Object> passwordValid(@RequestBody Map<String, String> request){
-        Map<String,String> result = new HashMap<>();
+    @PostMapping(value = "/confirmpassword/check")
+    public ResponseEntity<Object> passwordValid(@RequestBody Map<String, String> request) {
+        Map<String, String> result = new HashMap<>();
 
         try {
             String password = request.get("password");
-            String confirmPassword =request.get("confirmPassword");
+            String confirmPassword = request.get("confirmPassword");
             result.put("field", "confirmPassword");
 
-            if (userAccountService.isMatchPasswordAndConfirmPassword(password, confirmPassword )) {
+            if (userAccountService.isMatchPasswordAndConfirmPassword(password, confirmPassword)) {
                 throw new MismatchPasswordAndConfirmPassword();
 
             }
             result.put("status", "success");
             result.put("message", "비밀번호가 동일합니다.");
 
-            if(password.isEmpty() || password ==null){
-                result.put("message","");
+            if (password.isEmpty() || password == null) {
+                result.put("message", "");
             }
 
             return ResponseEntity.ok().body(result);
@@ -218,9 +248,9 @@ public class LocalUserAccountController {
         }
     }
 
-    @PostMapping(value="/nickname/check")
-    public ResponseEntity<Object> nicknameValid(@RequestBody Map<String, String> request){
-        Map<String,String> result = new HashMap<>();
+    @PostMapping(value = "/nickname/check")
+    public ResponseEntity<Object> nicknameValid(@RequestBody Map<String, String> request) {
+        Map<String, String> result = new HashMap<>();
 
         try {
 
@@ -228,14 +258,14 @@ public class LocalUserAccountController {
 
             result.put("field", "nickname");
 
-            if (userAccountService.isDuplicateNickname(nickname )) {
+            if (userAccountService.isDuplicateNickname(nickname)) {
                 throw new DuplicateNicknameException();
             }
             result.put("status", "success");
             result.put("message", "사용해도 되는 닉네임입니다.");
 
-            if(nickname.isEmpty() || nickname==null){
-                result.put("message","");
+            if (nickname.isEmpty() || nickname == null) {
+                result.put("message", "");
             }
 
             return ResponseEntity.ok().body(result);
@@ -249,16 +279,16 @@ public class LocalUserAccountController {
         }
     }
 
-    @PostMapping(value="/tel/check")
-    public ResponseEntity<Object> telValid(@RequestBody Map<String, String> request){
-        Map<String,String> result = new HashMap<>();
+    @PostMapping(value = "/tel/check")
+    public ResponseEntity<Object> telValid(@RequestBody Map<String, String> request) {
+        Map<String, String> result = new HashMap<>();
 
         try {
 
             String tel = request.get("tel");
 
             result.put("field", "tel");
-            System.out.println("tel:"+tel);
+            System.out.println("tel:" + tel);
 
             if (userAccountService.isDuplicateTel(tel)) {
                 throw new DuplicateTelException();
@@ -266,8 +296,8 @@ public class LocalUserAccountController {
             result.put("status", "success");
             result.put("message", "등록되지 않은 번호입니다. ");
 
-            if(tel.isEmpty() || tel==null){
-                result.put("message","");
+            if (tel.isEmpty() || tel == null) {
+                result.put("message", "");
             }
 
             return ResponseEntity.ok().body(result);
