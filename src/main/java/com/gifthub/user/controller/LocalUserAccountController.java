987d9cf579832fd9f4f3a40967c6d647f9444c40
9min9 <gming0226@gmail.com>
2023-12-11 15:37:53 +0900

    fix : Di successResponse in Controller

--- src/main/java/com/gifthub/user/controller/LocalUserAccountController.java
+++ src/main/java/com/gifthub/user/controller/LocalUserAccountController.java
@@ -4,13 +4,14 @@ import com.gifthub.config.jwt.LocalUserAuthenticationProvider;
 import com.gifthub.config.jwt.SocialAuthenticationToken;
 import com.gifthub.global.error.ErrorResponse;
 import com.gifthub.global.exception.ExceptionResponse;
+import com.gifthub.global.exception.RequiredFieldException;
+import com.gifthub.global.success.SuccessResponse;
 import com.gifthub.user.UserJwtTokenProvider;
 import com.gifthub.user.dto.LocalUserDto;
 import com.gifthub.user.dto.SignupRequest;
 import com.gifthub.user.dto.UserDto;
 import com.gifthub.user.entity.enumeration.LoginType;
 import com.gifthub.user.exception.*;
-import com.gifthub.user.exception.validator.LoginValidator;
 import com.gifthub.user.service.LocalUserService;
 import com.gifthub.user.service.UserAccountService;
 import com.gifthub.user.service.UserService;
@@ -48,6 +49,7 @@ public class LocalUserAccountController {
     private final LocalUserService localUserService;
     private final ErrorResponse errorResponse;
     private final ExceptionResponse exceptionResponse;
+    private final SuccessResponse successResponse;
 
     @PostMapping("/submit")
     public ResponseEntity<Object> signup(@Valid @RequestBody SignupRequest signupRequest, BindingResult bindingResult) {
@@ -125,13 +127,11 @@ public class LocalUserAccountController {
 
         } catch (NotFoundUserException e) {
             log.error("Login Controller | " + e);
-            Map<String, String> exception = exceptionResponse.getException("email", e.getCode(), e.getMessage());
-
-            return ResponseEntity.badRequest().body(exception);
+            return ResponseEntity.badRequest().body(exceptionResponse.getException("email", e.getCode(), e.getMessage()));
 
         } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.badRequest().build();
+            log.error("Login Controller | " + e);
+            return ResponseEntity.badRequest().body(exceptionResponse.getException(null, "Exception", e.getMessage()));
         }
 
         if (errors.hasErrors()) {
@@ -152,162 +152,130 @@ public class LocalUserAccountController {
         return ResponseEntity.ok().header("Authorization", "Bearer " + token).build();
     }
 
-    @PostMapping(value = "/email/check")
+    @PostMapping(value = "/validate/email")
     public ResponseEntity<Object> emailCheck(@RequestBody Map<String, String> request) {
-        Map<String, String> result = new HashMap<>();
-
         try {
             String email = request.get("email");
-            result.put("field", "email");
+
+            if(email.isEmpty() || email == null) {
+                throw new RequiredFieldException();
+            }
 
             if (userAccountService.isDuplicateEmail(email)) {
                 throw new DuplicateEmailException();
             }
 
-            result.put("status", "success");
-            result.put("message", "사용 가능한 이메일 입니다");
+            return ResponseEntity.ok().body(successResponse.getSuccess("email", "사용가능한 이메일 입니다"));
 
-            if (email.isEmpty() || email == null) {
-                result.put("message", "");
-            }
-
-            return ResponseEntity.ok().body(result);
+        } catch (RequiredFieldException e) {
+            return ResponseEntity.badRequest().body(exceptionResponse.getException("email", e.getCode(), e.getMessage()));
 
         } catch (DuplicateEmailException e) {
-            result.put("status", "error");
-            result.put("code", e.getCode());
-            result.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(exceptionResponse.getException("email", e.getCode(), e.getMessage()));
 
-            return ResponseEntity.badRequest().body(result);
         }
     }
 
-    @PostMapping(value = "/password/check")
+    @PostMapping(value = "/validate/password")
     public ResponseEntity<Object> passwordCheck(@RequestBody Map<String, String> request) {
         Map<String, String> result = new HashMap<>();
 
         try {
-
             String passwrod = request.get("password");
             String confirmPassword = request.get("confirmPassword");
-            result.put("field", "confirmPassword");
 
+            if (passwrod.isEmpty() || passwrod == null) {
+                //todo : 필수 에러 던지고 catch 하기
+//                throw new RequiredF
+            }
 
             if (!userAccountService.isMatchPasswordAndConfirmPassword(passwrod, confirmPassword)) {
                 throw new MismatchPasswordAndConfirmPassword();
             }
 
-            result.put("status", "success");
-            result.put("message", "비밀번호가 동일합니다.");
-
-            if (passwrod.isEmpty() || passwrod == null) {
-                result.put("message", "");
-            }
-
-
-            return ResponseEntity.ok().body(result);
+            return ResponseEntity.ok().body(successResponse.getSuccess("password", "비밀번호가 일치합니다"));
 
         } catch (MismatchPasswordAndConfirmPassword e) {
-            result.put("status", "error");
-            result.put("code", e.getCode());
-            result.put("message", e.getMessage());
-
-
-            return ResponseEntity.badRequest().body(result);
+            log.error("passwordCheck | " + e);
+            return ResponseEntity.badRequest().body(exceptionResponse.getException(e.getField(), e.getCode(), e.getMessage()));
         }
     }
 
-    @PostMapping(value = "/confirmpassword/check")
-    public ResponseEntity<Object> passwordValid(@RequestBody Map<String, String> request) {
-        Map<String, String> result = new HashMap<>();
+//    @PostMapping(value = "/confirmpassword/check")
+//    public ResponseEntity<Object> passwordValid(@RequestBody Map<String, String> request) {
+//        Map<String, String> result = new HashMap<>();
+//
+//        try {
+//            String password = request.get("password");
+//            String confirmPassword = request.get("confirmPassword");
+//            result.put("field", "confirmPassword");
+//
+//            if (userAccountService.isMatchPasswordAndConfirmPassword(password, confirmPassword)) {
+//                throw new MismatchPasswordAndConfirmPassword();
+//
+//            }
+//            result.put("status", "success");
+//            result.put("message", "비밀번호가 동일합니다.");
+//
+//            if (password.isEmpty() || password == null) {
+//                result.put("message", "");
+//            }
+//
+//            return ResponseEntity.ok().body(result);
+//
+//        } catch (MismatchPasswordAndConfirmPassword e) {
+//            result.put("status", "error");
+//            result.put("code", e.getCode());
+//            result.put("message", e.getMessage());
+//
+//            return ResponseEntity.badRequest().body(result);
+//        }
+//    }
 
-        try {
-            String password = request.get("password");
-            String confirmPassword = request.get("confirmPassword");
-            result.put("field", "confirmPassword");
-
-            if (userAccountService.isMatchPasswordAndConfirmPassword(password, confirmPassword)) {
-                throw new MismatchPasswordAndConfirmPassword();
-
-            }
-            result.put("status", "success");
-            result.put("message", "비밀번호가 동일합니다.");
-
-            if (password.isEmpty() || password == null) {
-                result.put("message", "");
-            }
-
-            return ResponseEntity.ok().body(result);
-
-        } catch (MismatchPasswordAndConfirmPassword e) {
-            result.put("status", "error");
-            result.put("code", e.getCode());
-            result.put("message", e.getMessage());
-
-            return ResponseEntity.badRequest().body(result);
-        }
-    }
-
-    @PostMapping(value = "/nickname/check")
+    @PostMapping(value = "/validate/nickname")
     public ResponseEntity<Object> nicknameValid(@RequestBody Map<String, String> request) {
         Map<String, String> result = new HashMap<>();
 
         try {
-
             String nickname = request.get("nickname");
-
-            result.put("field", "nickname");
-
-            if (userAccountService.isDuplicateNickname(nickname)) {
-                throw new DuplicateNicknameException();
-            }
-            result.put("status", "success");
-            result.put("message", "사용해도 되는 닉네임입니다.");
 
             if (nickname.isEmpty() || nickname == null) {
                 result.put("message", "");
             }
 
-            return ResponseEntity.ok().body(result);
+            if (userAccountService.isDuplicateNickname(nickname)) {
+                throw new DuplicateNicknameException();
+            }
+
+            return ResponseEntity.ok().body(successResponse.getSuccess("nickname", "사용가능한 닉네임 입니다"));
 
         } catch (DuplicateNicknameException e) {
-            result.put("status", "error");
-            result.put("code", e.getCode());
-            result.put("message", e.getMessage());
-
-            return ResponseEntity.badRequest().body(result);
+            return ResponseEntity.badRequest().body(exceptionResponse.getException(e.getField(), e.getCode(), e.getMessage()));
         }
     }
 
-    @PostMapping(value = "/tel/check")
+    @PostMapping(value = "/validate/tel")
     public ResponseEntity<Object> telValid(@RequestBody Map<String, String> request) {
-        Map<String, String> result = new HashMap<>();
-
         try {
-
             String tel = request.get("tel");
 
-            result.put("field", "tel");
-            System.out.println("tel:" + tel);
+            if (tel.isEmpty() || tel == null) {
+                throw new RequiredFieldException();
+            }
 
             if (userAccountService.isDuplicateTel(tel)) {
                 throw new DuplicateTelException();
             }
-            result.put("status", "success");
-            result.put("message", "등록되지 않은 번호입니다. ");
+            // todo : 패턴 검증
+            return ResponseEntity.ok().body(successResponse.getSuccess("tel", "사용 가능한 전화번호 입니다"));
 
-            if (tel.isEmpty() || tel == null) {
-                result.put("message", "");
-            }
-
-            return ResponseEntity.ok().body(result);
+        } catch (RequiredFieldException e) {
+            e.setFieldException("tel", "전화번호를 입력해주세요");
+            return ResponseEntity.badRequest().body(exceptionResponse.getException(e.getField(), e.getCode(), e.getMessage()));
 
         } catch (DuplicateTelException e) {
-            result.put("status", "error");
-            result.put("code", e.getCode());
-            result.put("message", e.getMessage());
-
-            return ResponseEntity.badRequest().body(result);
+            log.error("telValid | " +e);
+            return ResponseEntity.badRequest().body(exceptionResponse.getException(e.getField(), e.getCode(), e.getMessage()));
         }
     }
 
