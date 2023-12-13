55ca52dab8fb84f7b5a45e8acda9e579fd66edc3
9min9 <gming0226@gmail.com>
2023-12-14 00:21:31 +0900

    rename : ProductAddRequest
    Admin이 검수 후 등록되는 Gifticon, Product 이므로
    GifticonAppovalRequest가 적합하다고 생각되어 변경

--- src/main/java/com/gifthub/admin/dto/GifticonAppovalRequest.java
+++ src/main/java/com/gifthub/admin/dto/GifticonAppovalRequest.java
@@ -1,30 +1,20 @@
 package com.gifthub.admin.dto;
 
 import com.gifthub.product.dto.ProductDto;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
-public class ProductAddRequest {
-    @NotBlank
+public class GifticonAppovalRequest {
     private String productName;
-    @NotBlank
     private String brandName;
-    @NotBlank
     private String due;
-    @NotBlank
     private String barcode;
-    @NotBlank
     private String storageId;
-    @NotNull
     private Long price;
-    @NotNull
     private Boolean isConfirm;
-
     private String category;
     private String cancelReason;
 
