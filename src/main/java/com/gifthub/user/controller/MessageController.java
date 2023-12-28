3974fefcedd76c4cd369e6d0dc65ef93d8657360
jaehyeongP <asso03089@naver.com>
2023-12-28 17:34:45 +0900

    feat: MessageController.java 메세지 보내기 (MMS버전) 추가했습니다.

--- src/main/java/com/gifthub/user/controller/MessageController.java
+++ src/main/java/com/gifthub/user/controller/MessageController.java
@@ -5,11 +5,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.model.StorageType;
 import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import net.nurigo.sdk.message.service.DefaultMessageService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
@@ -27,18 +32,25 @@ public class MessageController {
 
     public MessageController() {
         this.messageService = NurigoApp.INSTANCE.initialize("NCSYGCMKQ4XE3UCI", "NDMX45JDHISWSGRCZIIVV9YJ7FRDBPBU", "https://api.coolsms.co.kr");
-//        this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecret, "https://api.coolsms.co.kr");
     }
 
     @PostMapping(value = "/check/sendSMS")
     @ResponseBody
-    SingleMessageSentResponse sendSMS(@RequestBody LocalUserDto localUserDto) {
+    SingleMessageSentResponse sendSMS(@RequestBody LocalUserDto localUserDto) throws IOException {
+        String path ="static/images/categorylogo/test.jpg";
+
+        //path 값에 이미지 path 작성하면 됩니다. 저장되있는 이미지만 보내는게 가능하고, 인터넷 주소에서 복사한 이미지로는 안됩니다.
+        ClassPathResource resource = new ClassPathResource(path);
+        File file = resource.getFile();
+        String imageId = messageService.uploadFile(file, StorageType.MMS, null);
         String tel = localUserDto.getTel().replace("-", "");
 
         Message message = new Message();
+        message.setSubject("제목");
         message.setFrom(masterphone);
         message.setTo(tel);
-        message.setText("https://myawsimgbucket.s3.ap-northeast-2.amazonaws.com/59c7a3ac-9b48-49de-9bf6-75f8b7022155.jpg");
+        message.setImageId(imageId);
+        message.setText("텍스트넣어주세요");
         log.info("MessageController | sendSMS | 정상 출력");
         SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
 
