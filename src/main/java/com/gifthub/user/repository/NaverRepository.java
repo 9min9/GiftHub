b5cf8ffaa8408e05b742c4e3bba029b7cac541ec
9min9 <gming0226@gmail.com>
2024-01-11 01:03:01 +0900

    remove : Not use java file

--- src/main/java/com/gifthub/user/repository/NaverRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gifthub.user.repository;
-
-import com.gifthub.user.entity.NaverUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NaverRepository  extends JpaRepository <NaverUser , Long> {
-}
